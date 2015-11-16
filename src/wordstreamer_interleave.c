b5f9135b483bf00c7ef2e337d45a288fb7a20690
Jean-Yves VET <contact@jean-yves.vet>
2015-11-16 11:05:03 +0100

    Interleave implementation for wordstreamer and unit tests

---
+++ src/wordstreamer_interleave.c
@@ -0,0 +1,202 @@
+/*******************************************************************************
+* Copyright (C) 2015, Jean-Yves VET, contact [at] jean-yves [dot] vet          *
+*                                                                              *
+* This software is licensed as described in the file LICENCE, which you should *
+* have received as part of this distribution. You may opt to use, copy,        *
+* modify, merge, publish, distribute and/or sell copies of the Software, and   *
+* permit persons to whom the Software is furnished to do so, under the terms   *
+* of the LICENCE file.                                                         *
+*                                                                              *
+* This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY    *
+* KIND, either express or implied.                                             *
+*******************************************************************************/
+
+/**
+ * @file wordstreamer_interleave.c
+ * @brief Streamer to retrieve words from a file. With multiple streams, file is
+          accessed in an interleaved fashion.
+ * @author Jean-Yves VET
+ */
+
+#include "wordstreamer_interleave.h"
+
+Wordstreamer* _mr_wordstreamer_interleave_create(const char*, char *, const int,
+                                                         const int, const bool);
+
+/* ========================= Constructor / Destructor ======================= */
+
+/**
+ * Constructor for the first streamer.
+ *
+ * @param   file_path[in]     String containing the path to the file to read
+ * @param   nb_streamers[in]  Total number of streamers
+ * @param   profiling[in]     Activate the profiling mode
+ * @return  Pointer to the new Wordstreamer structure
+ */
+Wordstreamer* mr_wordstreamer_interleave_create_first(const char* file_path,
+                                       const int nb_streamers, bool profiling) {
+    return _mr_wordstreamer_interleave_create(file_path, NULL, 0,
+                                                       nb_streamers, profiling);
+}
+
+
+/**
+ * Constructor for each other streamer.
+ *
+ * @param   first[in]        String containing the path to the file to read
+ * @param   streamer_id[in]  Total number of streamers
+ * @return  Pointer to the new Wordstreamer structure
+ */
+Wordstreamer* mr_wordstreamer_interleave_create_another(Wordstreamer* first,
+                                                        const int streamer_id) {
+
+    Wordstreamer* ws = _mr_wordstreamer_interleave_create("", first->shared_map,
+                            streamer_id, first->nb_streamers, first->profiling);
+
+    ws->file_size = first->file_size;
+
+    return ws;
+}
+
+
+/**
+ * Delete a Wordstreamer structure.
+ *
+ * @param   ws_ptr[in]   Pointer to the Wordstreamer structure
+ */
+void  mr_wordstreamer_interleave_delete(Wordstreamer* ws) {
+    _mr_wordstreamer_common_delete(ws);
+}
+
+
+/* ============================= Private functions ========================== */
+
+/**
+ * Internal constructor for Wordstreamer_interleave.
+ *
+ * @param   file_path[in]     String containing the path to the file to read
+ * @param   shared_map[in]    Pointer to area where the file was mmaped
+ * @param   streamer_id[in]   Id of the current wordstreamer
+ * @param   nb_streamers[in]  Total number of streamers
+ * @param   profiling[in]     Activate the profiling mode
+ * @return  Pointer to the new Wordstreamer structure
+ */
+Wordstreamer* _mr_wordstreamer_interleave_create(
+          const char* file_path, char *shared_map,
+          const int streamer_id, const int nb_streamers, const bool profiling) {
+
+    Wordstreamer *ws =  _mr_wordstreamer_common_create(file_path, shared_map,
+                                          streamer_id, nb_streamers, profiling);
+
+    ws->get = mr_wordstreamer_interleave_get;
+    ws->delete = mr_wordstreamer_interleave_delete;
+
+    ws->type = TYPE_WORDSTREAMER_INTERLEAVE;
+
+    ws->chunk_size = ws->file_size;
+    ws->start_offset = 0;
+    ws->offset = ws->start_offset;
+    ws->stop_offset = ws->file_size;
+
+    return ws;
+}
+
+
+/**
+ * Remove non-word characters to stream.
+ *
+ * @param   ws[inout]          Pointer to the Wordstreamer structure
+ * @param   file_size[in]      Size in Bytes of the input file
+ * @param   start_offset[in]   Start offset of the current chunk in Bytes
+ * @param   streamer_id[in]    Id of the current wordstreamer
+ * @param   shared_map[in]     Pointer to area where the file was mmaped
+ */
+static inline void _mr_wordstreamer_remove_nonwords(Wordstreamer *ws,
+                                                             char *shared_map) {
+    long int offset = ws->offset;
+    char character = shared_map[offset];
+
+    /* Remove extra spaces and punctuation char */
+    while (ispunct(character) || isspace(character)) {
+        character = shared_map[++offset];
+    }
+
+    ws->offset = offset;
+}
+
+
+/**
+ * Retrieve a word.
+ *
+ * @param   ws[inout]          Pointer to the Wordstreamer structure
+ * @param   file_size[in]      Size in Bytes of the input file
+ * @param   buffer[out]        Buffer to hold the retrieved word
+ * @param   streamer_id[in]    Id of the current wordstreamer
+ * @param   shared_map[in]     Pointer to area where the file was mmaped
+ */
+static inline void _mr_wordstreamer_retrieve_word(Wordstreamer *ws,
+                           long int file_size, char *buffer, char *shared_map) {
+    long int offset = ws->offset;
+    char character = shared_map[offset];
+    int i=0;
+
+    /* Retrieve all characters */
+    while(!ispunct(character) && !isspace(character) && offset < file_size) {
+        buffer[i++] = character;
+        character = shared_map[++offset];
+    }
+
+    /* Terminate string */
+    buffer[i] = '\0';
+
+    /* First char to lower case */
+    buffer[0] = tolower(buffer[0]);
+
+    ws->offset = offset;
+}
+
+
+/* ============================= Public functions =========================== */
+
+/**
+ * Get next word from a wordstreamer. Return 1 if end of stream reached.
+ *
+ * @param   ws[in]               Pointer to the Wordstreamer structure
+ * @param   shared_map[out]      Buffer to hold the retrieved word
+ * @return  0 if a word was copied into the buffer or 1 if the end of the stream
+ *          was reached
+ */
+int mr_wordstreamer_interleave_get(Wordstreamer *ws, char *buffer) {
+    _timer_start(&ws->timer_get);
+    char word[MAPREDUCE_MAX_WORD_SIZE];
+    int nb_streamers = ws->nb_streamers;
+    int streamer_id = ws->streamer_id;
+    char* map = ws->shared_map;
+    long int offset, file_size = ws->file_size;
+    int word_count = 0;
+
+    /* Return because end of stream already reached */
+    //if(ws->end) return 1;
+
+    /* Find the next word associated with the streamer id */
+    while (word_count < nb_streamers) {
+        /* Remove incomplete words, spaces and punctuation */
+        _mr_wordstreamer_remove_nonwords(ws, map);
+        offset = ws->offset;
+
+        if (offset < file_size-1) {
+            /* Retrieve a complete word */
+
+            _mr_wordstreamer_retrieve_word(ws, file_size, word, map);
+            if (word_count == streamer_id) strcpy(buffer, word);
+            word_count++;
+
+            //if (ws->offset >= stop_offset) ws->end = true;
+        } else {
+            break;
+        }
+    }
+
+    _timer_stop(&ws->timer_get);
+    return (word_count <= streamer_id);
+}
---
+++ tests/wordstreamer_interleave/test_wordstreamer_interleave.c
@@ -0,0 +1,205 @@
+/*******************************************************************************
+* Copyright (C) 2015, Jean-Yves VET, contact [at] jean-yves [dot] vet          *
+*                                                                              *
+* This software is licensed as described in the file LICENCE, which you should *
+* have received as part of this distribution. You may opt to use, copy,        *
+* modify, merge, publish, distribute and/or sell copies of the Software, and   *
+* permit persons to whom the Software is furnished to do so, under the terms   *
+* of the LICENCE file.                                                         *
+*                                                                              *
+* This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY    *
+* KIND, either express or implied.                                             *
+*******************************************************************************/
+
+#include "wordstreamer_interleave.h"
+#include <check.h>
+
+#define MAX_STREAMERS 11
+
+void create_file(const char *filename, const char *content) {
+    FILE *fp;
+    fp = fopen (filename,"w");
+    if (fp!=NULL) {
+        fprintf(fp, content);
+        fclose (fp);
+    }
+}
+
+
+START_TEST (test_create_delete)
+{
+    /* Create test file */
+    char *filename = "ws_test.txt";
+    char *content = "content tests";
+    create_file(filename, content);
+
+    Wordstreamer *ws = mr_wordstreamer_interleave_create_first(filename, 1,
+                                                                         false);
+    ck_assert_ptr_ne(ws, NULL);
+
+    ck_assert_int_eq(ws->nb_streamers, 1);
+    ck_assert_int_eq(ws->streamer_id, 0);
+    ck_assert_int_eq(ws->start_offset, 0);
+    ck_assert_int_eq(ws->profiling, 0);
+
+    mr_wordstreamer_interleave_delete(ws);
+
+    /* Delete testfile */
+    remove(filename);
+}
+END_TEST
+
+
+START_TEST (test_singlestreamer_get)
+{
+    /* Create test file */
+    char *filename = "ws_test.txt";
+    char *content = ".Donec!, ut  libero sed. ";
+    create_file(filename, content);
+
+    Wordstreamer *ws = mr_wordstreamer_interleave_create_first(filename, 1,
+                                                                         false);
+
+    char buffer[32];
+    int ret;
+
+    ret = mr_wordstreamer_interleave_get(ws, buffer);
+    ck_assert_int_eq(ret, 0);
+    ck_assert_str_eq(buffer, "donec");
+
+    ret = mr_wordstreamer_interleave_get(ws, buffer);
+    ck_assert_int_eq(ret, 0);
+    ck_assert_str_eq(buffer, "ut");
+
+    ret = mr_wordstreamer_interleave_get(ws, buffer);
+    ck_assert_int_eq(ret, 0);
+    ck_assert_str_eq(buffer, "libero");
+
+    ret = mr_wordstreamer_interleave_get(ws, buffer);
+    ck_assert_int_eq(ret, 0);
+    ck_assert_str_eq(buffer, "sed");
+
+    ret = mr_wordstreamer_interleave_get(ws, buffer);
+    ck_assert_int_eq(ret, 1);
+
+    mr_wordstreamer_interleave_delete(ws);
+
+    /* Delete testfile */
+    remove(filename);
+}
+END_TEST
+
+
+START_TEST (test_multiplestreamer_get)
+{
+    // Create test file
+    char *filename = "ws_test.txt";
+    char *content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+                    "Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas "
+                    "congue ligula ac quam viverra nec consectetur ante "
+                    "hendrerit. Donec et mollis dolor. Praesent et diam eget "
+                    "libero egestas mattis sit amet vitae augue. Nam tincidunt "
+                    "congue enim, ut porta lorem lacinia consectetur. Donec ut "
+                    "libero sed arcu vehicula ultricies a non tortor. Lorem "
+                    "ipsum dolor sit amet, consectetur adipiscing elit. Aenean "
+                    "ut gravida lorem. Ut turpis felis, pulvinar a semper sed, "
+                    "adipiscing id dolor. Pellentesque auctor nisi id magna "
+                    "consequat sagittis. Curabitur dapibus enim sit amet elit "
+                    "pharetra tincidunt feugiat nisl imperdiet. Ut convallis "
+                    "libero in urna ultrices accumsan. Donec sed odio eros. "
+                    "Donec viverra mi quis quam pulvinar at malesuada arcu "
+                    "rhoncus. Cum sociis natoque penatibus et magnis dis "
+                    "parturient montes, nascetur ridiculus mus. In rutrum "
+                    "accumsan ultricies. Mauris vitae nisi at sem facilisis "
+                    "semper ac in est.";
+
+    create_file(filename, content);
+
+    // Sequential streamer as a reference
+    Wordstreamer *ws_ref = mr_wordstreamer_interleave_create_first(filename, 1,
+                                                                         false);
+    char ref[4096];
+    char word[128];
+
+    if (!mr_wordstreamer_interleave_get(ws_ref, word)) strcpy(ref, word);
+
+    while (!mr_wordstreamer_interleave_get(ws_ref, word)) {
+        strcat(ref, " ");
+        strcat(ref, word);
+    }
+
+    mr_wordstreamer_interleave_delete(ws_ref);
+
+
+    // Check several streamers combination
+    for (int i=2; i<=MAX_STREAMERS; i++) {
+        char comp[4096];
+        int ret = 0;
+        int first = 1;
+
+        /* Create streamers */
+        Wordstreamer **ws = malloc(sizeof(Wordstreamer)*i);
+        ws[0] = mr_wordstreamer_interleave_create_first(filename, i, false);
+
+        for(int s=1; s<i; s++) {
+            ws[s] = mr_wordstreamer_interleave_create_another(ws[0], s);
+        }
+
+        /* Retrieve words */
+        while (! ret) {
+            for(int s=0; s<i; s++) {
+                if (!mr_wordstreamer_interleave_get(ws[s], word)) {
+                    if (first) {
+                        strcpy(comp, word);
+                        first = 0;
+                    } else {
+                        strcat(comp, " ");
+                        strcat(comp, word);
+                    }
+                } else {
+                    ret = 1;
+                }
+            }
+        }
+
+        for(int s=0; s<i; s++) {
+            mr_wordstreamer_interleave_delete(ws[s]);
+        }
+        free(ws);
+
+        ck_assert_str_eq(comp, ref);
+    }
+
+    remove(filename);
+}
+END_TEST
+
+
+Suite *wordstreamer_interleave_suite(void) {
+    Suite *suite = suite_create("Wordstreamer Interleave");
+    TCase *tcase1 = tcase_create("Case Create Delete");
+    TCase *tcase2 = tcase_create("Case Single streamer Get");
+    TCase *tcase3 = tcase_create("Case mutiple streamers Get");
+
+    tcase_add_test(tcase1, test_create_delete);
+    tcase_add_test(tcase2, test_singlestreamer_get);
+    tcase_add_test(tcase3, test_multiplestreamer_get);
+
+    suite_add_tcase(suite, tcase1);
+    suite_add_tcase(suite, tcase2);
+    suite_add_tcase(suite, tcase3);
+
+    return suite;
+}
+
+
+int main(void) {
+    int number_failed;
+    Suite *suite = wordstreamer_interleave_suite();
+    SRunner *runner = srunner_create(suite);
+
+    srunner_run_all(runner, CK_NORMAL);
+    number_failed = srunner_ntests_failed(runner);
+    srunner_free(runner);
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
